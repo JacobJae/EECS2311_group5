db24bda4862962c26834ba5ba7a4c11f8dbb4533
KarmitP98 <46545932+KarmitP98@users.noreply.github.com>
2019-01-13 23:04:12 -0500

    Added TalkBoxV1
    This one can be used for the Lab Tasks and Testing purposes.

---
+++ TalkBoxV1/src/pullPush/PullPushSample.java
@@ -0,0 +1,10 @@
+package pullPush;
+
+public class PullPushSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
