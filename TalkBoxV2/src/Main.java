61d77b08955d392f57104811fa32a743adf53e60
PeterSaleeb <peter.s1999@hotmail.com>
2019-01-14 12:08:40 -0500

    Initial Main class in Java 8

---
+++ TalkBoxV2/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Fun with Git!");
+		System.out.println("Testing");
+		System.out.println("Testing 1 2 3");
+	}
+
+}
