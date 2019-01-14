6094e25e8d302085b536ec2642c947341af5d3b6
PeterSaleeb <peter.s1999@hotmail.com>
2019-01-14 12:01:48 -0500

    Added third print message

--- TalkBox/src/Main.java
+++ TalkBox/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Fun with Git!");
 		System.out.println("Testing");
+		System.out.println("Testing 1 2 3");
 	}
 
 }
