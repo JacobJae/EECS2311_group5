60609f76b0ddb7087570135a062ede335ff9137c
KarmitP98 <karmi@192.168.0.15>
2019-01-17 10:04:05 -0500

    Added a basic jFrame

---
+++ TalkBoxV2/src/talkAppV1/TalkAppGUI.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package talkAppV1;
+
+import javax.swing.JFrame;
+
+/**
+ * @author karmi
+ *
+ */
+public class TalkAppGUI extends JFrame {
+
+	/**
+	 * @param args
+	 */
+
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
+
---
+++ TalkBoxV2/src/talkAppV1/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author karmi
+ *
+ */
+package talkAppV1;
+
+// This package will be used to create the TalkBox app version 1.
