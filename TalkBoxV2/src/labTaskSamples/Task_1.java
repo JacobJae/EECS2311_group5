b94018165715967299492f63e9dc0925cce9e306
KarmitP98 <kk07@my.yorku.ca>
2019-01-21 12:04:06 -0500

    Wrong change push

--- TalkBoxV2/src/labTaskSamples/Task_1.java
+++ TalkBoxV2/src/labTaskSamples/Task_1.java
@@ -5,5 +5,7 @@ public class Task_1 {
 	public static void main(String[] arg) {
 		System.out.print("Creating conflict");
 		System.out.println("Only Branch should change!");
+		
+		System.out.println("NO CHANGE");
 	}
 }
