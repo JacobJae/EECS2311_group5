66aa18b972ab519cbb182bbb6b5e98dc9b56e40d
KarmitP98 <karmi@192.168.0.17>
2019-01-21 08:02:50 -0500

    Design as well as functoin changes.

--- TalkBoxV2/src/talkAppV1/TalkBoxGui.java
+++ TalkBoxV2/src/talkAppV1/TalkBoxGui.java
@@ -14,6 +14,7 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -30,14 +31,16 @@ import javax.swing.JTree;
 
 public class TalkBoxGui extends JFrame {
 
-	private JLabel disp;
-	private JButton b1, b2, b3, b4;
+	private JLabel Display;
+	private JButton btnSound1, btnSound2, btnSound3, btnSound4, btnPlaySound;
 	private JPanel contentPane;
 	private File audio;
 	private String name, no = "TalkBoxData/no.wav", strong_no = "TalkBoxData/strong_no.wav",
 			yes = "TalkBoxData/yes.wav", hell_yeah = "TalkBoxData/hell_yeah.wav";
 	private AudioInputStream audioIn;
 	private Clip clip = null;
+	private ArrayList<String> clips = new ArrayList();
+	private ArrayList<JButton> sButtons = new ArrayList();
 
 	/**
 	 * Launch the application.
@@ -59,93 +62,124 @@ public class TalkBoxGui extends JFrame {
 	 * Create the frame.
 	 */
 	public TalkBoxGui() {
-		setAlwaysOnTop(true);
 
+		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 711, 551);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		// this.setLocationRelativeTo(null);
-		// this.setExtendedState(MAXIMIZED_BOTH);
+
+		init();
+		addComp();
+
+		btnSound1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectBtn(0);
+				//playSound(no);
+				//Display.setText(name + " Pressed!");
+			}
+		});
+
+		btnSound2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectBtn(1);
+				//playSound(strong_no);
+				//Display.setText(name + " Pressed!");
+			}
+		});
+
+		btnSound3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				selectBtn(2);
+				//playSound(yes);
+				//Display.setText(name + " Pressed!");
+			}
+		});
+
+		btnSound4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				selectBtn(3);
+				//playSound(hell_yeah);
+				//Display.setText(name + " Pressed!");
+			}
+		});
+
+		btnPlaySound.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for(int i=0;i<sButtons.size();i++)
+				{
+					if(sButtons.get(i).isSelected())
+					{
+						playSound(clips.get(i));
+						Display.setText(name + " Pressed!");						
+					}						
+				}
+				
+			}
+		});
+		
+	}
+
+	private void selectBtn(int x) {
+		
+		for (int i = 0; i < sButtons.size(); i++) {
+			if (i == x)
+				sButtons.get(i).setSelected(true);
+			else
+				sButtons.get(i).setSelected(false);
+		}
+		
+	}
+
+	private void addComp() {
+		clips.add(no);
+		clips.add(strong_no);
+		clips.add(yes);
+		clips.add(hell_yeah);
+
+		sButtons.add(btnSound1);
+		sButtons.add(btnSound2);
+		sButtons.add(btnSound3);
+		sButtons.add(btnSound4);
+
+	}
+
+	private void init() {
 
 		getClip();
 
-		JLabel Display = new JLabel("BUTTON PRESSED!");
+		Display = new JLabel("BUTTON PRESSED!");
+		contentPane.add(Display);
 		Display.setHorizontalAlignment(SwingConstants.CENTER);
 		Display.setFont(new Font("Stencil", Font.BOLD, 48));
 		Display.setBounds(10, 301, 670, 200);
-		contentPane.add(Display);
 
-		JButton btnImage1 = new JButton("NO");
-		btnImage1.setBounds(10, 175, 160, 100);
-		btnImage1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				playSound(no);
-				Display.setText(name + " Pressed!");
-			}
-		});
-		contentPane.add(btnImage1);
+		btnSound1 = new JButton("NO");
+		btnSound1.setFont(new Font("Stencil", Font.BOLD, 18));
+		contentPane.add(btnSound1);
+		btnSound1.setBounds(10, 11, 160, 100);
 
-		JButton btnImage2 = new JButton("Strong NO");
-		btnImage2.setBounds(180, 175, 160, 100);
-		btnImage2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				playSound(strong_no);
-				Display.setText(name + " Pressed!");
-			}
-		});
-		btnImage2.setActionCommand("IMAGE 1");
-		contentPane.add(btnImage2);
+		btnSound2 = new JButton("Strong NO");
+		btnSound2.setFont(new Font("Stencil", Font.BOLD, 18));
+		contentPane.add(btnSound2);
+		btnSound2.setBounds(180, 11, 160, 100);
 
-		JButton btnImage3 = new JButton("YES");
-		btnImage3.setBounds(350, 175, 160, 100);
-		btnImage3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				playSound(yes);
-				Display.setText(name + " Pressed!");
-			}
-		});
-		contentPane.add(btnImage3);
+		btnSound3 = new JButton("YES");
+		btnSound3.setFont(new Font("Stencil", Font.BOLD, 18));
+		contentPane.add(btnSound3);
+		btnSound3.setBounds(350, 11, 160, 100);
 
-		JButton btnImageSimulator = new JButton("Simulator");
-		btnImageSimulator.setEnabled(false);
-		btnImageSimulator.setBounds(520, 175, 160, 100);
-		btnImageSimulator.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Simulator simulator = new Simulator(contentPane);
-				simulator.setVisible(true);
-			}
-		});
-		contentPane.add(btnImageSimulator);
+		btnSound4 = new JButton("HELL YEAH");
+		btnSound4.setFont(new Font("Stencil", Font.BOLD, 18));
+		contentPane.add(btnSound4);
+		btnSound4.setBounds(520, 11, 160, 100);
 
-		JButton btnImage4 = new JButton("HELL YEAH");
-		btnImage4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				playSound(hell_yeah);
-				Display.setText(name + " Pressed!");
-			}
-		});
-		btnImage4.setBounds(10, 48, 160, 100);
-		contentPane.add(btnImage4);
-
-		// Needs some work.
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				if (arg0.equals(KeyEvent.VK_N)) {
-					playSound(no);
-					Display.setText(name + " Pressed!");
-				} else if (arg0.equals(KeyEvent.VK_Y)) {
-					playSound(yes);
-					Display.setText(name + " Pressed!");
-				} else {
-					playSound(strong_no);
-					Display.setText(name + " Pressed!");
-				}
-			}
-		});
+		btnPlaySound = new JButton("Play Sound");
+		btnPlaySound.setFont(new Font("Stencil", Font.BOLD, 20));
+		btnPlaySound.setBounds(10, 233, 670, 100);
+		contentPane.add(btnPlaySound);
 	}
 
 	private void getClip() {
@@ -167,10 +201,10 @@ public class TalkBoxGui extends JFrame {
 	}
 
 	private void playSound(String audioFile) {
-		
+
 		clip.stop();
 		clip.close();
-		
+
 		getName(audioFile);
 		audio = new File(audioFile);
 		try {
@@ -188,7 +222,7 @@ public class TalkBoxGui extends JFrame {
 
 	private void setVolume() {
 		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-		double gain = 0.25;  
+		double gain = 0.25;
 		float dB = (float) (Math.log(gain) / Math.log(10.0) * 20.0);
 		gainControl.setValue(dB);
 	}
