22fd6c38694ece6c7313820c9e7a51b1f90b6ecc
JacobJae <kses1234@my.yorku.ca>
2019-01-21 11:45:02 -0500

    Creating Conflict

--- TalkBoxV2/src/labTaskSamples/Task_1.java
+++ TalkBoxV2/src/labTaskSamples/Task_1.java
@@ -3,7 +3,7 @@ package labTaskSamples;
 public class Task_1 {
 
 	public static void main(String[] arg) {
-		System.out.print("Hello World");
+		System.out.print("Creating conflict");
 		System.out.println("Only Branch should change!");
 	}
 }
