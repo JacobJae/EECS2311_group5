fa566eba2b90162ec1ccc5fba3ec69cca0292dc9
JacobJae <kses1234@my.yorku.ca>
2019-01-21 12:07:34 -0500

    Line 6 push

--- TalkBoxV2/src/labTaskSamples/Task_1.java
+++ TalkBoxV2/src/labTaskSamples/Task_1.java
@@ -3,7 +3,7 @@ package labTaskSamples;
 public class Task_1 {
 
 	public static void main(String[] arg) {
-		System.out.println("Solving conflict!!!!!!!!!");
+		System.out.println("asdsadasdsa");
 		System.out.print("Creating conflict");
 		System.out.println("Only Branch should change!");
 		
