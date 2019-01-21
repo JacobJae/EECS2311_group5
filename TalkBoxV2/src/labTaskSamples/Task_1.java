34c480d82640e7bb178c5ff5f4e2ff0b96fe5a19
KarmitP98 <kk07@my.yorku.ca>
2019-01-21 12:14:31 -0500

    Labtest2 push

--- TalkBoxV2/src/labTaskSamples/Task_1.java
+++ TalkBoxV2/src/labTaskSamples/Task_1.java
@@ -5,5 +5,9 @@ public class Task_1 {
 	public static void main(String[] arg) {
 		System.out.print("Creating");
 		System.out.println("Only Branch should change!");
+		
+		
+		
+		System.out.println("Added a statement in Labtest2 branch!");
 	}
 }
