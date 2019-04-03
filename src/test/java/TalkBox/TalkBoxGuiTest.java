c1f1cfe0a810af93a3dc06289de6589a723eefd1
JacobJae <kses1234@my.yorku.ca>
2019-04-03 02:47:55 -0400

    Tests added

---
+++ src/test/java/TalkBox/TalkBoxGuiTest.java
@@ -0,0 +1,61 @@
+package test.java.TalkBox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import main.java.TalkBox.model.TalkBox;
+import main.java.TalkBox.view.TalkBoxGui;
+
+class TalkBoxGuiTest {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		TalkBox box = new TalkBox();
+		TalkBoxGui gui = new TalkBoxGui();
+	}
+	
+//	@org.junit.jupiter.api.Test
+//	void test7() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test8() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test9() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test10() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test11() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test12() {
+//		fail("placeholder");
+//	}
+//
+//	@org.junit.jupiter.api.Test
+//	void test13() {
+//		fail("placeholder");
+//	}
+}
